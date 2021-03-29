88fcbad1205c8481e530d5438e4219b6bffea1a3
Noah Solomon <noahsolomon0518@gmail.com>
2021-03-29 12:48:27 -0400

    finished multinet implementation

---
+++ midi_parser/multi_net/__init__.py
@@ -0,0 +1,1 @@
+from .multi_net_encoder import MidiToDecimalMultiNet
---
+++ test/multi_net/test_multi_net_encoder.py
@@ -0,0 +1,33 @@
+from midi_parser.multi_net import MidiToDecimalMultiNet
+from midi_parser import Player
+from midi_parser.sampler import SmpFile
+
+
+
+import unittest
+
+
+
+encoderMultiNet = MidiToDecimalMultiNet("C:/Users/noahs/Data Science/Music Generation AI/data/testing",  debug = True, maxOctaves=4, smallestTimeUnit=  1/32)
+encoderMultiNet.encode()
+encodedOTs = encoderMultiNet.encoded
+
+
+
+class TestOTEncoderOnOnly(unittest.TestCase):
+    
+
+    def test_lengths(self):
+        
+        print(encodedOTs)
+        self.assertGreater(len(encodedOTs), 0)
+        for OT in encodedOTs:
+            self.assertGreater(len(OT), 0)
+            for note in OT:
+                self.assertGreater(note, 0)
+  
+        
+        
+
+
+
